c6341199d16a8ff955b829fcf8a00d1137c393ed
Jesús López <jesus.lopez@datons.ai>
2024-06-05 11:23:01 +0200

    Aplicación lista para ser publicada.

--- notebooks/10_Streamlit/app.py
+++ notebooks/10_Streamlit/app.py
@@ -2,27 +2,31 @@ import pandas as pd
 import pickle
 import streamlit as st
 
-with open('../artifacts/model.pkl', 'rb') as f:
-    model = pickle.load(f)
-
-with st.sidebar.form(key='house_price'):
+# Load trained ML model to make predictions on new data
+## Form data
+with st.sidebar.form('price'):
     
     your_house = {
-        'BEDROOMS': st.number_input('BEEDROOMS', value=3),
-        'BATHROOMS': st.number_input('BATHROOMS', value=2),
+        'BEDROOMS': st.number_input('BEDROOMS', 3),
+        'BATHROOMS': st.number_input('BATHROOMS', 2),
         'GARAGE': st.number_input('GARAGE', 2),
         'FLOOR_AREA': st.number_input('FLOOR_AREA', 200),
-        'BUILD_YEAR': st.number_input('BUILD_YEAR', 2000),
+        'BUILD_YEAR': st.number_input('BUILD_YEAR', 2000)
     }
     
-    name = st.text_input('Address', placeholder='Wall Street')
-    submit = st.form_submit_button('Submit')
+    submit = st.form_submit_button('Calculate')
 
-if submit:
+name = 'Wall Street'
+df_house = pd.DataFrame(your_house, index=[name])
+
+## Load trained model
+
+with open('artifacts/model.pkl', 'rb') as f:
+    model = pickle.load(f)
     
-    df_house = pd.DataFrame(your_house, index=[name])
-    st.write('For a house with the following features:')
-    st.write(df_house)
-    
-    y_pred = model.predict(df_house)[0]
-    st.write(f'The estimated price is ${y_pred:,.2f}')
+## Calculate prediction
+y_pred = model.predict(df_house)[0]
+
+st.write(f'The estimated price is ${y_pred:,}')
+st.write('Given the features of your house:')
+df_house
