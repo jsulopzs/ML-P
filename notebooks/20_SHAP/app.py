2c9c9358370068665af4c00256df4574d827db31
Jesús López <jesus.lopez@datons.ai>
2024-06-05 13:26:59 +0200

    Aplicación con valores SHAP.

--- notebooks/20_SHAP/app.py
+++ notebooks/20_SHAP/app.py
@@ -1,41 +1,53 @@
 import pandas as pd
 import pickle
 import streamlit as st
-
-import streamlit.components.v1 as components
 import shap
+import streamlit.components.v1 as components
+import json
+
+## Load trained model
+import os
+
+dir = os.path.dirname(os.path.abspath(__file__))
+path_model = os.path.join(dir, 'artifacts/model.pkl')
+
+with open(path_model, 'rb') as f:
+    model = pickle.load(f)
+
+path_explainer = os.path.join(dir, 'artifacts/explainer.pkl')
+with open(path_explainer, 'rb') as f:
+    explainer = pickle.load(f)
+    
+path_options = os.path.join(dir, 'src/options.json')
+with open(path_options, 'r') as f:
+    options = json.load(f)
+    
+# Load trained ML model to make predictions on new data
+## Form data
+## DRY: DON'T REPEAT YOURSELF
+
+with st.sidebar.form('price'):
+    
+    your_house = {}
+    for k,v in options.items():
+        your_house[k] = st.number_input(k, value=v)
+    
+    submit = st.form_submit_button('Calculate')
+
+name = 'Wall Street'
+df_house = pd.DataFrame(your_house, index=[name])
+    
+## Calculate prediction
+y_pred = model.predict(df_house)[0]
+
+st.write(f'The estimated price is ${y_pred:,}')
+st.write('Given the features of your house:')
+
+shap_values = explainer.shap_values(df_house)
 
 def st_shap(plot, height=None):
     shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
     components.html(shap_html, height=height)
-
-with open('artifacts/model.pkl', 'rb') as f:
-    model = pickle.load(f)
-
-with open('artifacts/explainer.pkl', 'rb') as f:
-    explainer = pickle.load(f)
     
-with st.sidebar.form(key='house_price'):
-    
-    your_house = {
-        'BEDROOMS': st.number_input('BEEDROOMS', value=3),
-        'BATHROOMS': st.number_input('BATHROOMS', value=2),
-        'GARAGE': st.number_input('GARAGE', 2),
-        'FLOOR_AREA': st.number_input('FLOOR_AREA', 200),
-        'BUILD_YEAR': st.number_input('BUILD_YEAR', 2000),
-    }
-    
-    name = st.text_input('Address', placeholder='Wall Street')
-    submit = st.form_submit_button('Submit')
-
-if submit:
-    
-    df_house = pd.DataFrame(your_house, index=[name])
-    y_pred = model.predict(df_house)[0]
-    
-    st.write(f'The estimated price is ${y_pred:,.2f}')
-        
-    shap_values = explainer.shap_values(df_house)
-    st.write('Based on the model, the influence of each feature is:')
-    fp = shap.force_plot(explainer.expected_value, shap_values, df_house)
-    st_shap(fp, height=300)
+fp = shap.force_plot(explainer.expected_value, shap_values, df_house)
+st_shap(fp)
